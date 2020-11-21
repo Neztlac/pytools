c214e2f97e5cfb294d267ce18473d14c464f2fb3
Neztlac <hugo.brezet@gmail.com>
2020-11-21 01:59:46 +0100

    Removed 'pyFunc' ; Added 'fishAliasList.sh'

---
+++ fishAliasList.sh
@@ -0,0 +1,24 @@
+/usr/bin/fish
+
+echo $SHELL
+
+#SOFTWARE
+alias v="nvim"
+alias py="python3"
+alias ssavr="firefox ssavr.com"
+
+#LINUX COMMANDS
+alias rmr="rm -r"
+alias install="sudo apt -y install"
+alias uu="sudo apt -y update && sudo apt -y upgrade"
+alias shutdown="sudo shutdown now"
+
+#FUNCSAVE
+
+funcsave v
+funcsave py
+funcsave ssavr
+funcsave rmr
+funcsave install
+funcsave uu
+funcsave shutdown
